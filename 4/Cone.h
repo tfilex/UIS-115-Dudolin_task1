a37fdd3ac2fc945eca15df80e035307625fb5142
Dudolin Deins <80900158+tfilex@users.noreply.github.com>
2021-05-19 10:37:48 +0300

    Добавлена документация в Cone.h

--- 4/Cone.h
+++ 4/Cone.h
@@ -1,22 +1,59 @@
 #pragma once
 #include <cmath>
 
-const double M_PI = 3.14159265359;
 class Cone
 {
 protected:
+        /**
+	*\brief константа π
+	*/
 	const double M_PI = 3.14159265359;
+
+        /**
+	*\brief высота
+	*/
 	double height;
+
+        /**
+	*\brief радиус
+	*/
 	double radius;
+
+        /**
+	*\brief сеттер для проверки параметров
+	*/
 	void set_cone(double height, double radius);
 public:
+        /**
+	*\brief конструктор по умолчанию
+	*/
 	Cone();
+        /**
+	*\brief конструктор с параметрами
+	* \param высота
+	* \param радиус
+	*/
 	Cone(double height, double radius);
 
+
+        /**
+	*\brief деструктор по умолчанию
+	*/
 	~Cone() = default;
 
+
+        /**
+	*\brief метод, вычисляющий объём конуса
+	* \param высота
+	* \param радиус
+	*/
 	double get_volume(double height, double radius);
 
+        /**
+	*\brief метод, вычисляющий площадь поверхности
+	* \param высота
+	* \param радиус
+	*/
 	double get_surface_area(double height, double radius);
 
 };
