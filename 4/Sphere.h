b9d004358b05ce00dc6f16d3a8bf2e4bf753ae67
Dudolin Deins <80900158+tfilex@users.noreply.github.com>
2021-05-19 11:54:23 +0300

    Добавлена документация в Sphere.h

--- 4/Sphere.h
+++ 4/Sphere.h
@@ -4,14 +4,32 @@
 class Sphere : public Cone
 {
 private:
+        /**
+	*\brief радиус
+	*/
 	double radius;
 public:
+        /**
+	*\brief конструктор с параметрами
+	* \param радиус
+	*/
+	explicit Sphere(double radius);
 
-	Sphere(double radius);
-
+        /**
+	*\brief деструктор по умолчанию
+	*/
 	~Sphere() = default;
 
+
+        /**
+	*\brief метод, возвращающий объём шара
+	* \param радиус
+	*/
 	double get_volume(double radius);
 
+        /** метод, возвращающий площадь поверхности шара
+	*\brief 
+	* \param радиус
+	*/
 	double get_surface_area(double radius);
 };
