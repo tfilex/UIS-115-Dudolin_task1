86cea2a800ad95426cbdf365fdeea7911aefe3cb
Dudolin Deins <80900158+tfilex@users.noreply.github.com>
2021-05-19 10:44:18 +0300

    Сделана документация в Pyramid.h

--- 4/Pyramid.h
+++ 4/Pyramid.h
@@ -4,18 +4,58 @@
 class Pyramid : public Cone
 {
 private:
+        /**
+	*\brief сторона a
+	*/
 	double a;
+
+        /**
+	*\brief сторона b
+	*/
 	double b;
+
+        /**
+	*\brief сторона c
+	*/
 	double c;
+
+        /**
+	*\brief высота
+	*/
 	double height;
 public:
+        /**
+	*\brief конструктор с параметрами
+	* \param сторона a
+	* \param сторона b
+	* \param сторона c
+	* \param высота
+	*/
 	explicit Pyramid(double a, double b, double c, double height);
 
+
+        /**
+	*\brief деструктор по умолчанию
+	*/
 	~Pyramid() = default;
 
+
+        /**
+	*\brief метод, возвращающий объём пирамиды
+	* \param сторона a
+	* \param сторона b
+	* \param сторона c
+	* \param высота
+	*/
 	double get_volume(double a, double b, double c, double height);
 
-
+        /**
+	*\brief метод, возвращающий площадь поверхности пирамиды
+	* \param сторона a
+	* \param сторона b
+	* \param сторона c
+	* \param высота
+	*/
 	double get_surface_area(double a, double b, double c, double height);
 
 
